807bfccfb0104486c98bb66a81bf6b4541103e4b
tsunamifi <104786105+tsunamifi@users.noreply.github.com>
2022-05-18 16:01:09 -0500

    Update tsuMadlibs.py

--- tsuMadlibs.py
+++ tsuMadlibs.py
@@ -63,7 +63,7 @@ def madlib1():
         submit = st.form_submit_button('Submit')
     with Cover:
         st.image(c1)
-        if submit:
+     if submit:
         st.write(textwrap.fill(stry, 150))
         
    
@@ -90,7 +90,7 @@ def  madlib2():
         submit = st.form_submit_button('Submit')
     with Cover2:
         st.image(c2)
-        if submit:
+      if submit:
           st.write(textwrap.fill(stry, 80))
 
         
